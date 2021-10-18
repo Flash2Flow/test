b49bca277d779578f802ee9a3a5ef4c7a53ccc32
re-incarnation <re-incarnation@bk.ru>
2021-10-18 11:32:57 +1000

    test

---
+++ auth.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"context"
+	"github.com/go-session/session"
+)
+
+func auth(page http.ResponseWriter, req *http.Request) {
+	login := req.FormValue("login")
+	password := req.FormValue("password")
+
+	store, err := session.Start(context.Background(), page, req)
+	if err != nil {
+		fmt.Fprint(page, err)
+		return
+	}
+
+	if login == "test"{
+		if password == "1234"{
+			store.Set("active_login", login)
+			err = store.Save()
+			if err != nil {
+				fmt.Fprint(page, err)
+				return
+			}
+			http.Redirect(page, req, "/", 302)
+		}else{
+			fmt.Fprintf(page, "Err password")
+		}
+	}else{
+		fmt.Fprintf(page, "Err login")
+	}
+}
---
+++ main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/go-session/session"
+	"log"
+	"net/http"
+	"os"
+	"html/template"
+
+)
+
+
+func main() {
+	//design pages
+	http.HandleFunc("/", home)
+
+	//tech pages
+	http.HandleFunc("/auth", auth)
+
+	port := os.Getenv("PORT")
+	log.Println("start server with port: " +port)
+	http.ListenAndServe(":"+port, nil)
+}
+
+
+
+func home(page http.ResponseWriter, req *http.Request) {
+	store, err := session.Start(context.Background(), page, req)
+	if err != nil {
+		fmt.Fprint(page, err)
+		return
+	}
+	temp, err := template.ParseFiles("index.html")
+
+	if err != nil {
+		fmt.Fprintf(page, err.Error())
+	}
+
+	temp.ExecuteTemplate(page, "home_page", nil)
+
+	active, ok := store.Get("active_login")
+	if ok {
+		fmt.Fprintf(page, "Nice session %s", active)
+	}else{
+		fmt.Fprintf(page, "Error session" )
+	}
+
+
+}

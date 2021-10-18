6e0abfdf1614b549d2fee458f76d87192d0bc3da
re-incarnation <re-incarnation@bk.ru>
2021-10-18 15:32:35 +1000

    as

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,14 @@
+$(window).on('load', function () {
+  $('#load').delay(17500).fadeOut('slow');
+
+  $('#example3').typeIt({
+    strings: ["Добро пожаловать на Бета-Тест сайта сервера по майкнрафту.", "SWORD ART ONLINE", "После прогрузки сайта,авторизируйтесь или зарегистрируйтесь", "затем, нажмите кнопку Home, что бы вызвать панель.", "Если у вас ноутбук,зажмите клавишу *Fn и стрелку в лево*"],
+    speed: 50,
+    autoStart: false
+  });
+});
+
+
 document.addEventListener('keyup', function(){
 if (event.keyCode == 36){
 $( "#container" ).stop().toggle();
